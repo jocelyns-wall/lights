dd6ca1d9e52f94fa6241c274c0d3232744d00664
jocelyns-wall <113924258+jocelyns-wall@users.noreply.github.com>
2022-09-19 23:10:00 +0100

    connection setup and special effects messages done

--- lights.ts
+++ lights.ts
@@ -1,41 +1,56 @@
-
 /**
-* Use this file to define custom functions and blocks.
-* Read more at https://makecode.microbit.org/blocks/custom
-*/
-
-/**
- * Different things that can happen on Jocelyn's wall
+ * Different effects that can happen on Jocelyn's wall
  */
-enum Happening {
-    //% block="rainbow"
-    "Rainbow",
-    //% block="stripes"
-    "Stripes"
+enum SpecialEffect {
+    Rainbow,
+    //% block="Rainbow with Glitter"
+    RainbowWithGlitter,
+    Confetti,
+    //% block="Sweeping Dot"
+    Sweepingdot,
+    //% block="Pulsing Stripes"
+    PulsingStripes,
+    //% block="Juggling Dots"
+    JugglingDots,
+}
+
+function effectStr(effect: SpecialEffect): string {
+    switch (effect) {
+        case SpecialEffect.Rainbow: return "RAINBOW";
+        case SpecialEffect.RainbowWithGlitter: return "RAINBGL";
+        case SpecialEffect.Confetti: return "CONFETT";
+        case SpecialEffect.Sweepingdot: return "SWEEPDT";
+        case SpecialEffect.PulsingStripes: return "PSTRIPE";
+        case SpecialEffect.JugglingDots: return "JUGGDOT";
+    }
 }
 
 /**
  * lights blocks
  */
-//% weight=100 color=#f9ae00 icon=""
+//% weight=1000 color=#f9ae00 icon=""
 namespace jC_Lights {
+
     /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
+     * Sets up a bluetooth radio connection with another
+     * microbit within the wall.
+     * The microbit communicates with a raspberry pi
+     * which communicates in turn with an arduino!
      */
-    //% block
-    export function foo(n: number, s: string): String {
-        return "Hello"
+    //% block="Set up a connection with Jocelyn's wall"
+    export function setWallConnection():void {
+        radio.setGroup(111)
+        serial.redirectToUSB()
     }
 
     /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
+     * Sends a special effect to Jocelyn's wall
+     * over bluetooth and serial channels
      */
-    //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value - 1) + fib(value - 2);
+    //% block="send a %effect special effect to Jocelyn's wall || for %duration seconds"
+    export function specialEffect(effect: SpecialEffect, duration: number=0): void {
+        const message = "EFFECT:" + effectStr(effect) + ":" + duration
+        radio.sendString(message)
+        serial.writeLine(message)
     }
 }
--- main.ts
+++ main.ts
@@ -1,1 +1,4 @@
-
+input.onButtonPressed(Button.A, function () {
+    jC_Lights.specialEffect(SpecialEffect.Rainbow)
+})
+jC_Lights.setWallConnection()
