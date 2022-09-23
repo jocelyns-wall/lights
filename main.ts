6275d8df60f2dd27df1028d77c8a8f51e6028167
jocelyns-wall <113924258+jocelyns-wall@users.noreply.github.com>
2022-09-23 18:38:51 +0100

    complete test in main

--- main.ts
+++ main.ts
@@ -1,7 +1,27 @@
 input.onButtonPressed(Button.A, function () {
-    jC_Lights.sendWallCommand(jC_Lights.specialEffectCommand(SpecialEffect.Rainbow))
+    effect += 1
+    if (effect > 6) {
+        effect = 1
+    }
+    if (effect == 1) {
+        command = jC_Lights.specialEffectCommand(SpecialEffect.Rainbow)
+    } else if (effect == 2) {
+        command = jC_Lights.specialEffectCommand(SpecialEffect.RainbowWithGlitter)
+    } else if (effect == 3) {
+        command = jC_Lights.specialEffectCommand(SpecialEffect.Confetti)
+    } else if (effect == 4) {
+        command = jC_Lights.specialEffectCommand(SpecialEffect.Sweepingdot)
+    } else if (effect == 5) {
+        command = jC_Lights.specialEffectCommand(SpecialEffect.PulsingStripes)
+    } else if (effect == 6) {
+        command = jC_Lights.specialEffectCommand(SpecialEffect.JugglingDots)
+    }
+    basic.showString("" + (effect))
 })
 input.onButtonPressed(Button.B, function () {
-    jC_Lights.sendWallCommand(jC_Lights.specialEffectCommand(SpecialEffect.Sweepingdot))
+    jC_Lights.sendWallCommand(command)
 })
+let command = ""
+let effect = 0
 jC_Lights.setWallConnection()
+effect = 0
