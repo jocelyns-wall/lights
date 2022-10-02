009e6da50f4496dadced8f18eb8def06b20c738e
jocelyns-wall <113924258+jocelyns-wall@users.noreply.github.com>
2022-10-02 20:55:33 +0100

    cut down radio messages to under 12 characters

--- main.ts
+++ main.ts
@@ -1,14 +1,4 @@
 input.onButtonPressed(Button.A, function () {
-    do_brightness = true
+    jC_Lights.sendWallCommand(jC_Lights.doEffectDemo())
 })
-input.onButtonPressed(Button.B, function () {
-    do_brightness = false
-})
-let do_brightness = false
 jC_Lights.setWallConnection()
-do_brightness = false
-loops.everyInterval(100, function () {
-    if (do_brightness == true) {
-        jC_Lights.sendWallCommand(jC_Lights.setBrightness(Math.map(input.acceleration(Dimension.X), -1023, 1023, 0, 255)))
-    }
-})
